edd31e8cbe15f91f469d1c51c475a40f690569c1
putrianugrahesodik <putrianugrahesodik@gmail.com>
2025-09-16 10:17:11 +0700

    Resep baru

---
+++ Resep.java
@@ -0,0 +1,7 @@
+public class Resep {public static void main(String[] args) {
+    System.out.println("Resep Krabby Patty");
+    System.out.println("---------------");
+    System.out.println("1. ketimun");
+    System.out.println("2. bawang");
+}
+}
